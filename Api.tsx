81d8f5ae8121cdfbb841edd4ffe026ce98dfb273
Luca <46204097+luca-parlapiano@users.noreply.github.com>
2023-01-29 23:53:12 +0100

    First try

---
+++ Api.tsx
@@ -0,0 +1,3 @@
+'https://api.github.com/users/luca-parlapiano';
+'https://api.github.com/repos/luca-parlapiano/stargazersLuca';
+'https://api.github.com/repos/mattiaferigutti/Backdrop-Android/stargazers';
---
+++ Constants.tsx
@@ -0,0 +1,3 @@
+export const Constants = {
+  GITHUB_API: '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',
+};
